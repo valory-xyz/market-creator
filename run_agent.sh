5b68f86cc18e956e6ea66e24d089a096ac12d8e4
angrybayblade <vptl185@gmail.com>
2023-07-12 14:41:33 +0000

    chore: run agent script

--- run_agent.sh
+++ run_agent.sh
@@ -1,8 +1,8 @@
 rm -r agent
-find . -empty -type d -delete  # remove empty directories to avoid wrong hashes
+find . -empty -type d -delete # remove empty directories to avoid wrong hashes
 autonomy packages lock
 autonomy fetch --local --agent valory/market_maker --alias agent && cd agent
-autonomy generate-key ethereum
+cp $(pwd)/../ethereum_private_key.txt ethereum_private_key.txt
 autonomy add-key ethereum ethereum_private_key.txt
 aea install
 autonomy issue-certificates
