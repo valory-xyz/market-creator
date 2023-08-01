24012132680444a1723b4769ddf1a20149b63730
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2023-08-01 16:09:41 +0200

    chore: Update scripts

--- run_agent.sh
+++
@@ -1,9 +0,0 @@
-rm -r agent
-find . -empty -type d -delete # remove empty directories to avoid wrong hashes
-autonomy packages lock
-autonomy fetch --local --agent valory/market_maker --alias agent && cd agent
-cp $(pwd)/../ethereum_private_key.txt ethereum_private_key.txt
-autonomy add-key ethereum ethereum_private_key.txt
-aea install
-autonomy issue-certificates
-aea -s run
