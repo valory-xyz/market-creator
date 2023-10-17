95059f81e26d0def121b1d0ba30a507dfaf2be45
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2023-10-17 22:07:35 +0200

    [no CI] chore: hashes

--- run_service.sh
+++ run_service.sh
@@ -14,11 +14,10 @@ export ENGINE="gpt-4"
 export MARKET_APPROVAL_SERVER_URL=YOUR_MARKET_APPROVAL_SERVER_URL
 export MARKET_APPROVAL_SERVER_API_KEY=YOUR_MARKET_APPROVAL_SERVER_API_KEY
 
-export NUM_MARKETS=10
+export MIN_MARKET_PROPOSAL_INTERVAL_SECONDS=1800
 export TOPICS='["business","science","technology","politics","arts","weather"]'
 export MARKET_FEE=2
 export INITIAL_FUNDS=1
-export MINIMUM_MARKET_TIME=1
 export MARKET_TIMEOUT=1
 export MARKET_IDENTIFICATION_PROMPT=$(sed -e ':a' -e 'N' -e '$!ba' \
   -e 's/"/\\"/g' \
