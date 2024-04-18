3fd19f6ff4ca7a7af702ad9226a797f0b8681c0d
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2024-04-18 21:27:15 +0200

    feat: retry answers

--- packages/valory/skills/market_creation_manager_abci/behaviours.py
+++ packages/valory/skills/market_creation_manager_abci/behaviours.py
@@ -2291,6 +2291,53 @@ class GetPendingQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
         balance = cast(int, ledger_api_response.state.body.get("get_balance_result"))
         return balance
 
+    def _eligible_questions_to_answer(
+        self, unanswered_questions: List[Dict[str, Any]]
+    ) -> List[str]:
+        """Determine the eligible questions to answer at this time"""
+        now = self.last_synced_timestamp
+        eligible_questions_id = []
+
+        self.context.logger.info(
+            f"Answer retry intervals: {self.params.answer_retry_intervals}"
+        )
+
+        for question in unanswered_questions:
+            if question in self.shared_state.questions_responded:
+                continue
+
+            question_id = question["question"]["id"].lower()
+            if question_id not in self.shared_state.questions_requested_mech:
+                self.shared_state.questions_requested_mech[question_id] = {}
+                self.shared_state.questions_requested_mech[question_id][
+                    "question"
+                ] = question
+                self.shared_state.questions_requested_mech[question_id]["retries"] = []
+
+            retries = self.shared_state.questions_requested_mech[question_id]["retries"]
+            n_retries = len(retries)
+            time_since_last_retry = now - retries[-1] if retries else 0
+            retry_period = self.params.answer_retry_intervals[
+                min(n_retries, len(self.params.answer_retry_intervals) - 1)
+            ]
+            if n_retries == 0 or time_since_last_retry > retry_period:
+                eligible_questions_id.append(question_id)
+
+        self.context.logger.info(
+            f"Determined {len(eligible_questions_id)} eligible questions to answer."
+        )
+
+        num_questions = min(
+            len(eligible_questions_id), self.params.multisend_batch_size
+        )
+        random.seed(self.last_synced_timestamp)
+        random_questions_id = random.sample(eligible_questions_id, num_questions)
+
+        self.context.logger.info(
+            f"Chosen {len(random_questions_id)} eligible questions to answer."
+        )
+        return random_questions_id
+
     def get_payload(self) -> Generator[None, None, str]:
         """Get the transaction payload"""
         # get the questions to that need to be answered
@@ -2300,23 +2347,15 @@ class GetPendingQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
             self.context.logger.info("Couldn't get the questions")
             return GetPendingQuestionsRound.ERROR_PAYLOAD
 
-        filtered_questions = [
-            question
-            for question in unanswered_questions
-            if question["question"]["id"].lower()
-            not in self.shared_state.questions_requested_mech
-        ]
-        random.seed(self.last_synced_timestamp)
-        num_questions = min(len(filtered_questions), self.params.multisend_batch_size)
-        random_questions = random.sample(filtered_questions, num_questions)
-        questions = random_questions
+        eligible_questions_id = self._eligible_questions_to_answer(unanswered_questions)
 
-        if len(questions) == 0:
-            self.context.logger.info("No questions to close")
+        if len(eligible_questions_id) == 0:
+            self.context.logger.info("No eligible questions to answer")
             return GetPendingQuestionsRound.NO_TX_PAYLOAD
 
         self.context.logger.info(
-            f"Got {len(questions)} questions to close. " f"Questions: {questions}"
+            f"Got {len(eligible_questions_id)} questions to close. "
+            f"Questions ID: {eligible_questions_id}"
         )
 
         safe_address = self.synchronized_data.safe_contract_address
@@ -2327,7 +2366,7 @@ class GetPendingQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
 
         self.context.logger.info(f"Address {safe_address!r} has balance {balance}.")
         max_num_questions = min(
-            len(questions), self.params.questions_to_close_batch_size
+            len(eligible_questions_id), self.params.questions_to_close_batch_size
         )
         bond_required = self.params.close_question_bond * max_num_questions
 
@@ -2342,14 +2381,17 @@ class GetPendingQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
 
         # Prepare the Mech Requests for these questions
         new_mech_requests = []
-        for question in questions:
-            question_id = question["question"]["id"].lower()
-            if question_id in self.shared_state.questions_requested_mech:
-                # we already processed this question, skip it
-                self.context.logger.info(
-                    f"Question {question_id} already processed, skipping it."
-                )
-                continue
+        for question_id in eligible_questions_id:
+            question = self.shared_state.questions_requested_mech[question_id][
+                "question"
+            ]
+            retries = self.shared_state.questions_requested_mech[question_id]["retries"]
+            retries.append(self.last_synced_timestamp)
+
+            self.context.logger.info(
+                f"Requesting mech answer for question {question_id} ({question['title']})."
+            )
+            self.context.logger.info(f"Question {question_id} retries: {retries}.")
 
             new_mech_requests.append(
                 asdict(
@@ -2360,7 +2402,6 @@ class GetPendingQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
                     )
                 )
             )
-            self.shared_state.questions_requested_mech[question_id] = question
 
         self.context.logger.info(f"new_mech_requests: {new_mech_requests}")
 
@@ -2432,9 +2473,20 @@ class AnswerQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
             if question_id not in self.shared_state.questions_requested_mech:
                 continue
 
-            question = self.shared_state.questions_requested_mech[question_id]
-
+            question = self.shared_state.questions_requested_mech[question_id][
+                "question"
+            ]
+            retries = self.shared_state.questions_requested_mech[question_id]["retries"]
             answer = self._parse_mech_response(response)
+
+            if answer is None and len(retries) >= len(
+                self.params.answer_retry_intervals
+            ):
+                self.context.logger.info(
+                    f"Question {question} has been retried at timestamps {retries} without success. Assuming question is invalid."
+                )
+                answer = ANSWER_INVALID
+
             self.context.logger.info(f"Got answer {answer} for question {question}")
 
             if answer is None:
@@ -2469,6 +2521,7 @@ class AnswerQuestionsBehaviour(MarketCreationManagerBaseBehaviour):
             # try to answer the same question multiple times due to a out-of-sync issue
             # between the subgraph and the realitio contract.
             self.shared_state.questions_responded.add(question_id)
+            del self.shared_state.questions_requested_mech[question_id]
 
         if len(txs) == 0:
             # something went wrong, respond with ERROR payload for now
