4dec95775ae440d108bff1fe2a1552571359ad31
Anna Sambrook <annasambrook@gmail.com>
2025-03-27 12:08:41 +0000

    refactor: format remove unused imports sync markets

--- packages/valory/skills/market_creation_manager_abci/behaviours/sync_markets.py
+++ packages/valory/skills/market_creation_manager_abci/behaviours/sync_markets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2023-2024 Valory AG
+#   Copyright 2023-2025 Valory AG
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -20,61 +20,17 @@
 """This package contains sync markets behaviours of MarketCreationManagerAbciApp."""
 
 import json
-import random
-import time
-from abc import ABC
-from collections import defaultdict
-from dataclasses import asdict
 from datetime import datetime
-from string import Template
 from typing import (
     Any,
-    Callable,
     Dict,
     Generator,
     List,
-    Optional,
-    Set,
     Tuple,
-    Type,
     cast,
 )
-
-import packages.valory.skills.market_creation_manager_abci.propose_questions as mech_tool_propose_questions
-import packages.valory.skills.mech_interact_abci.states.request as MechRequestStates
-from packages.valory.contracts.conditional_tokens.contract import (
-    ConditionalTokensContract,
-)
 from packages.valory.contracts.fpmm.contract import FPMMContract
-from packages.valory.contracts.fpmm_deterministic_factory.contract import (
-    FPMMDeterministicFactory,
-)
-from packages.valory.contracts.gnosis_safe.contract import (
-    GnosisSafeContract,
-    SafeOperation,
-)
-from packages.valory.contracts.multisend.contract import (
-    MultiSendContract,
-    MultiSendOperation,
-)
-from packages.valory.contracts.realitio.contract import RealitioContract
-from packages.valory.contracts.wxdai.contract import WxDAIContract
 from packages.valory.protocols.contract_api import ContractApiMessage
-from packages.valory.protocols.ledger_api import LedgerApiMessage
-from packages.valory.protocols.llm.message import LlmMessage
-from packages.valory.skills.abstract_round_abci.base import AbstractRound
-from packages.valory.skills.abstract_round_abci.behaviours import (
-    AbstractRoundBehaviour,
-    BaseBehaviour,
-)
-from packages.valory.skills.abstract_round_abci.common import (
-    RandomnessBehaviour,
-    SelectKeeperBehaviour,
-)
-from packages.valory.skills.abstract_round_abci.models import Requests
-from packages.valory.skills.market_creation_manager_abci import (
-    PUBLIC_ID as MARKET_CREATION_MANAGER_PUBLIC_ID,
-)
 from packages.valory.skills.market_creation_manager_abci.behaviours.base import (
     FPMM_POOL_MEMBERSHIPS_QUERY,
     MarketCreationManagerBaseBehaviour,
@@ -82,49 +38,11 @@ from packages.valory.skills.market_creation_manager_abci.behaviours.base import 
     _ONE_DAY,
     get_callable_name,
 )
-from packages.valory.skills.market_creation_manager_abci.dialogues import LlmDialogue
-from packages.valory.skills.market_creation_manager_abci.models import (
-    MarketCreationManagerParams,
-    SharedState,
-)
 from packages.valory.skills.market_creation_manager_abci.payloads import (
-    AnswerQuestionsPayload,
-    ApproveMarketsPayload,
-    CollectProposedMarketsPayload,
-    DepositDaiPayload,
-    GetPendingQuestionsPayload,
-    PostTxPayload,
-    RedeemBondPayload,
-    RemoveFundingPayload,
     SyncMarketsPayload,
 )
 from packages.valory.skills.market_creation_manager_abci.rounds import (
-    AnswerQuestionsRound,
-    ApproveMarketsRound,
-    CollectProposedMarketsRound,
-    CollectRandomnessPayload,
-    CollectRandomnessRound,
-    DepositDaiRound,
-    GetPendingQuestionsRound,
-    MarketCreationManagerAbciApp,
-    PostTransactionRound,
-    PrepareTransactionPayload,
-    PrepareTransactionRound,
-    RedeemBondRound,
-    RemoveFundingRound,
-    RetrieveApprovedMarketPayload,
-    RetrieveApprovedMarketRound,
-    SelectKeeperPayload,
-    SelectKeeperRound,
     SyncMarketsRound,
-    SynchronizedData,
-)
-from packages.valory.skills.mech_interact_abci.states.base import (
-    MechInteractionResponse,
-    MechMetadata,
-)
-from packages.valory.skills.transaction_settlement_abci.payload_tools import (
-    hash_payload_to_hex,
 )
 
 
